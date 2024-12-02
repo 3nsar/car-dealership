fa180224da6d3747bbdd69d6a708582dea323ce2
3nsar <84131267+3nsar@users.noreply.github.com>
2024-12-02 22:18:56 +0100

    commit

---
+++ car-dealership/src/components/Navbar.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-scroll';
+import { motion } from 'framer-motion';
+
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleNavClick = () => {
+    setShowNav(!showNav);
+  };
+
+  const closeNav = () => {
+    setShowNav(false);
+  };
+
+  return (
+    <div className="navbar-container">
+      <motion.h1
+        initial={{ opacity: 0, scale: 0, y: -10 }}
+        whileInView={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{ duration: 0.4, type: 'spring' }}
+        viewport={{ once: true }}
+      >San Car </motion.h1>
+
+      <div className="nav-icon" onClick={handleNavClick}>
+        {showNav ? <AiOutlineClose fontSize="25px" /> : <FaBars fontSize="25px" />}
+      </div>
+
+      <ul className={`navbar-list ${showNav ? 'show' : ''}`}>
+      <Link
+            activeClass="active"
+            to="about-container"
+            smooth={true}
+            duration={500}
+            onClick={closeNav}
+            className="nav-link"
+      >
+        <li>
+          HOME
+        </li>
+
+        </Link>
+
+        <Link
+            activeClass="active"
+            to="projects-container"
+            smooth={true}
+            duration={500}
+            onClick={closeNav}
+            className="nav-link"
+      >
+        <li>
+          AUTOS
+        </li>
+
+        </Link>
+
+        <Link
+            activeClass="active"
+            to="skills-container"
+            smooth={true}
+            duration={500}
+            onClick={closeNav}
+            className="nav-link"
+      >
+        <li>
+          KONTAKT
+        </li>
+
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
