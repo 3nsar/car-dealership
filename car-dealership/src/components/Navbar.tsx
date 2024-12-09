850bd827187776d26116a7af25dd75f120683d52
3nsar <84131267+3nsar@users.noreply.github.com>
2024-12-09 22:20:50 +0100

    commit

--- car-dealership/src/components/Navbar.tsx
+++ car-dealership/src/components/Navbar.tsx
@@ -1,78 +1,58 @@
-import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
-import { AiOutlineClose } from 'react-icons/ai';
-import { Link } from 'react-scroll';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react'
+import {Menu, X} from 'lucide-react';
 
 const Navbar = () => {
-  const [showNav, setShowNav] = useState(false);
 
-  const handleNavClick = () => {
-    setShowNav(!showNav);
-  };
+  const [mobileDrawerOpen, setMobileDrawerOpen ] = useState(false);
 
-  const closeNav = () => {
-    setShowNav(false);
-  };
+  const toggleNavbar = ()=>{
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  }
 
   return (
-    <div className="navbar-container">
-      <motion.h1
-        initial={{ opacity: 0, scale: 0, y: -10 }}
-        whileInView={{ opacity: 1, scale: 1, y: 0 }}
-        transition={{ duration: 0.4, type: 'spring' }}
-        viewport={{ once: true }}
-      >San Car </motion.h1>
+    <nav className=' sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 '>
+        <div className=' text-gray-200 container px-4 mx-auto relative text-sm'>
+            <div className="flex justify-between items-center">
+                <div className="flex justify-center items-center space-x-5 mx-3">
+                    <span className='text-xl tracking-tight text-yellow-400 font-dothan'>San Car</span>
+                </div>
+                <ul className='hidden lg:flex ml-14 space-x-12'>
+                  <li> <a href="#">Home</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Pricing</a></li>
+                </ul>
+                <div className=" hidden lg:flex justify-center items-center space-x-5 mx-3">
+                <a href="#" className='py-2 px-3 text-center rounded-md bg-gradient-to-r from-teal-600 via-teal-500 to-teal-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300'>
+                    Kontaktiere uns
+                  </a>
+                </div>
+                <div className="lg:hidden md:flex flex-col justify-end">
+                  <button onClick={toggleNavbar}>
+                    {mobileDrawerOpen ? <X/> : <Menu/>}
+                  </button>
+                </div>
+            </div>
+            {mobileDrawerOpen && (
+              <div className="lg:hidden fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center">
+                  <ul>
+                    <li className='py-4'><a href="#">Home</a></li>
+                    <li className='py-4'><a href="#">Contact</a></li>
+                    <li className='py-4'><a href="#">Contact</a></li>
+                    <li className='py-4'><a href="#">Pricing</a></li>
+                  </ul>
+                  <div className="flex flex-col justify-center ">
+                  <a href="#" className='py-1 px-2 text-center rounded-md bg-gradient-to-r from-teal-600 via-teal-500 to-teal-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300'>
+                    Kontaktiere uns
+                  </a>
 
-      <div className="nav-icon" onClick={handleNavClick}>
-        {showNav ? <AiOutlineClose fontSize="25px" /> : <FaBars fontSize="25px" />}
-      </div>
+                  </div>
+              </div>
+            )}
 
-      <ul className={`navbar-list ${showNav ? 'show' : ''}`}>
-      <Link
-            activeClass="active"
-            to="about-container"
-            smooth={true}
-            duration={500}
-            onClick={closeNav}
-            className="nav-link"
-      >
-        <li>
-          HOME
-        </li>
+        </div>
+    </nav>
+  )
+}
 
-        </Link>
-
-        <Link
-            activeClass="active"
-            to="projects-container"
-            smooth={true}
-            duration={500}
-            onClick={closeNav}
-            className="nav-link"
-      >
-        <li>
-          AUTOS
-        </li>
-
-        </Link>
-
-        <Link
-            activeClass="active"
-            to="skills-container"
-            smooth={true}
-            duration={500}
-            onClick={closeNav}
-            className="nav-link"
-      >
-        <li>
-          KONTAKT
-        </li>
-
-        </Link>
-      </ul>
-    </div>
-  );
-};
-
-export default Navbar;
+export default Navbar
