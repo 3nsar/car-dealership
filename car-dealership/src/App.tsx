e3ab050ee66af10f4e489884c0cf10a657733a12
3nsar <84131267+3nsar@users.noreply.github.com>
2025-02-09 22:41:05 +0100

    commit

--- car-dealership/src/App.tsx
+++ car-dealership/src/App.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Termin from './components/Termin'
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
       {/*<Navbar/> */}
       <Hero/>
       <About/>
-      
+      <Termin/>
     </div>
   )
 }
---
+++ car-dealership/src/components/Termin.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Termin = () => {
+  return (
+    <div className="w-full h-screen flex items-center text-zinc-900 bg-white">
+      <h1>Termin</h1>
+      <button>Termin vereinbaren</button>
+    </div>
+  )
+}
+
+export default Termin
